ddc55c966f33181bb5ee5c938de123a9909ebcfe
Denali Molitor <dmolitor@ucla.edu>
2020-11-19 09:24:23 -0800

    test with channel

--- run-tests.bash
+++ run-tests.bash
@@ -2,20 +2,20 @@
 
 set -x
 
-if ! grep '^status = true$' <(./glasgow_subgraph_solver --format lad test-instances/small test-instances/large ) ; then
-    echo "non-induced test failed" 1>&1
-    exit 1
-fi
-
-if ! grep '^status = false$' <(./glasgow_subgraph_solver --induced --format lad test-instances/small test-instances/large ) ; then
-    echo "induced test failed" 1>&1
-    exit 1
-fi
-
-if ! grep '^solution_count = 6$' <(./glasgow_subgraph_solver --count-solutions --format lad test-instances/small test-instances/large ) ; then
-    echo "non-induced enumerate test failed" 1>&1
-    exit 1
-fi
+# if ! grep '^status = true$' <(./glasgow_subgraph_solver --format lad test-instances/small test-instances/large ) ; then
+#     echo "non-induced test failed" 1>&1
+#     exit 1
+# fi
+#
+# if ! grep '^status = false$' <(./glasgow_subgraph_solver --induced --format lad test-instances/small test-instances/large ) ; then
+#     echo "induced test failed" 1>&1
+#     exit 1
+# fi
+#
+# if ! grep '^solution_count = 6$' <(./glasgow_subgraph_solver --count-solutions --format lad test-instances/small test-instances/large ) ; then
+#     echo "non-induced enumerate test failed" 1>&1
+#     exit 1
+# fi
 
 if ! grep '^solution_count = 12$' <(./glasgow_subgraph_solver --count-solutions test-instances/trident.csv test-instances/longtrident.csv ) ; then
     echo "trident enumerate test failed" 1>&1
@@ -27,5 +27,9 @@ if ! grep '^solution_count = 6$' <(./glasgow_subgraph_solver --count-solutions -
     exit 1
 fi
 
-true
+if ! grep '^solution_count = 6$' <(./glasgow_subgraph_solver --count-solutions --induced test-instances/c3_with_labels.csv test-instances/c3c2_with_labels.csv ) ; then
+    echo "induced cyclic enumerate test failed" 1>&1
+    exit 1
+fi
 
+true
