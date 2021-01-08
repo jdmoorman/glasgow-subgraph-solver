66af19c0cda246b1a2c0649a7c67e9c736adf315
Denali Molitor <dmolitor@ucla.edu>
2021-01-08 13:46:58 -0800

    remove another has_stored_edges

--- src/formats/input_graph.hh
+++ src/formats/input_graph.hh
@@ -26,7 +26,7 @@ class InputGraph
         /**
          * \param initial_size can be 0, if resize() is called afterwards.
          */
-        InputGraph(int initial_size, bool has_stored_edges, bool has_vertex_labels, bool has_edge_labels, bool directed, bool loopy);
+        InputGraph(int initial_size, bool has_vertex_labels, bool has_edge_labels, bool directed, bool loopy);
 
         InputGraph(const InputGraph &) = delete;
 
