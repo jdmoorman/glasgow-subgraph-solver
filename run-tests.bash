288fc64edcf1ae5f2f53a7dc21d82b72fc38933c
Denali Molitor <dmolitor@ucla.edu>
2021-01-31 14:36:54 -0800

    remove lad format tests.

--- run-tests.bash
+++ run-tests.bash
@@ -2,21 +2,6 @@
 
 set -x
 
-# if ! grep '^status = true$' <(./glasgow_subgraph_solver --format lad test-instances/small test-instances/large ) ; then
-#     echo "non-induced test failed" 1>&1
-#     exit 1
-# fi
-#
-# if ! grep '^status = false$' <(./glasgow_subgraph_solver --induced --format lad test-instances/small test-instances/large ) ; then
-#     echo "induced test failed" 1>&1
-#     exit 1
-# fi
-#
-# if ! grep '^solution_count = 6$' <(./glasgow_subgraph_solver --count-solutions --format lad test-instances/small test-instances/large ) ; then
-#     echo "non-induced enumerate test failed" 1>&1
-#     exit 1
-# fi
-
 if ! grep '^solution_count = 12$' <(./glasgow_subgraph_solver --count-solutions test-instances/trident.csv test-instances/longtrident.csv ) ; then
     echo "trident enumerate test failed" 1>&1
     exit 1
